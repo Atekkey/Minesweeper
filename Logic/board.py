4418cfd9b564af21ebc409895661d05c9e37ee85
Anand <108430320+Atekkey@users.noreply.github.com>
2023-08-29 00:06:40 -0500

    a

--- Logic/board.py
+++ Logic/board.py
@@ -4,3 +4,21 @@
 #Assign mines
 #!!Assign numbers!!
 
+import random
+import math
+class Board():
+    def __init__(self, size, mine):
+        self.board = [[0 for i in range(size[0])] for j in range(size[1])]
+        self.mine = mine
+    def __str__(self):
+        temp = ""
+        for row in self.board:
+            for col in row:
+                temp += " " + str(col)
+            temp += "\n"
+        temp += "Mines : " + str(self.mine)
+        return temp
+
+#MAIN
+myBoard = Board((2,2), 10)
+print(myBoard)
