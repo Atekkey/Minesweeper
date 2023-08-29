8878607f399aff42c9e83be79302d6b8d67d6c93
Atekkey <anand@tekkey.net>
2023-08-29 15:18:55 -0500

    COMPLETE?

--- Logic/board.py
+++ Logic/board.py
@@ -8,6 +8,8 @@ import random
 class Board():
 
     def __init__(self, size, mine): #Constructor. If mine<=0, gives a random mine#
+        self.gameOver = False
+        self.chainedList = []
         self.board = [[0 for i in range(size[0])] for j in range(size[1])]
         self.shown = [[False for i in range(size[0])] for j in range(size[1])] #Visibility mask
         if(mine <= 0):
@@ -25,9 +27,15 @@ class Board():
         return temp
     
     def showShown(self): #Shows what is visible to player
-        temp = ""
+        temp = "# "
         for row in range(len(self.board)):
+            if(row == 0):
+                for col in range(len(self.board[row])):
+                    temp += str(col) + "|"
+                temp += "\n"
             for col in range(len(self.board[row])):
+                if(col == 0):
+                    temp += str(row) + "-"
                 if(self.shown[row][col] == True):
                     temp += str(self.board[row][col]) + " "
                 else:
@@ -69,15 +77,10 @@ class Board():
             myList.append((row+1, col))
         return myList
             
-        
-        
     
     def populate(self): #Populate the numbers
         print(" ")
-        #Iterate thru the X's or thru the 0's?
-        #Edge cases (literally)
-
-        #0 iter
+        #Iterate thru 0's
         for row in range(len(self.board)):
             for col in range(len(self.board[row])):
                 if(self.board[row][col] == "X"):
@@ -86,11 +89,50 @@ class Board():
                 for k in myTuples:
                     if(self.board[k[0]][k[1]] == "X"):
                         self.board[row][col] += 1
+
+    def chain(self, myRowCol):
+        showList = self.validTuples((myRowCol))
+        for k in showList:
+            if (k in self.chainedList):
+                continue
+            self.chainedList.append(k)
+            row, col = k[0], k[1]
+            self.shown[row][col] = True
+            if(self.board[row][col] == 0):
+                self.chain((row,col))
+
+    def ask(self):
+        got = input("Give a row and col in form #x# \n")
+        got = got.split("x")
+        row, col = int(got[0]), int(got[1])
+        self.shown[row][col] = True
+        if(self.board[row][col] == "X"): #LOSE HERE
+            self.gameOver = True
+            print(self)
+            print("You Lost!")
+            exit(1)
+        elif(self.board[row][col] == 0):
+            self.chain((row,col))
+
+    def checkWin(self):
+        for row in range(len(self.board)):
+            for col in range(len(self.board[row])):
+                if(self.board[row][col] != "X" and self.shown[row][col] == False):
+                    self.gameOver = False
+                    return
+        self.gameOver = True
+        return
+
     def main(self):
         self.plant()
         self.populate()
+        while(not self.gameOver):
+            print(self.showShown())
+            self.ask()
+            self.checkWin() #Win is outside, Loss terminates program
         print(self)
+        print("You won!")
 
 #MAIN
-myBoard = Board(size = (10,10), mine = 0)
+myBoard = Board(size = (10,10), mine = 10)
 myBoard.main()
